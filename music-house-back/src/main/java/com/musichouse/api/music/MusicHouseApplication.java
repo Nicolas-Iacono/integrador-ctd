524b0876328a730fa19982c812d75f1cfd385fa2
victor javier Vivas <victorjaviervivas@gmail.com>
2024-05-08 10:20:25 -0400

    Configuración de LOGGER y carpetas iniciales

--- music-house-back/src/main/java/com/musichouse/api/music/MusicHouseApplication.java
+++ music-house-back/src/main/java/com/musichouse/api/music/MusicHouseApplication.java
@@ -1,13 +1,17 @@
 package com.musichouse.api.music;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicHouseApplication {
+	private  static  final Logger LOGGER = LoggerFactory.getLogger(MusicHouseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicHouseApplication.class, args);
+		LOGGER.info("🎹 Let's hit the keys and start the musical journey with MusicHouseApplication! 🎵🌟");
 	}
 
 }

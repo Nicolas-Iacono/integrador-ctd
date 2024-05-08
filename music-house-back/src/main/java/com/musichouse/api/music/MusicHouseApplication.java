d5299a5ae9adac0d1d75a66d27ff775061952f81
victor javier Vivas <victorjaviervivas@gmail.com>
2024-05-08 09:51:29 -0400

    Inicio de proyecto integardor backend app: Music House

---
+++ music-house-back/src/main/java/com/musichouse/api/music/MusicHouseApplication.java
@@ -0,0 +1,13 @@
+package com.musichouse.api.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicHouseApplication.class, args);
+	}
+
+}

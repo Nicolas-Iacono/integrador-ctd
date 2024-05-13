db47eb2c98aac2ac2c19046f37b939bf42329289
victor javier Vivas <victorjaviervivas@gmail.com>
2024-05-13 11:55:01 -0400

    se incorpora la tabala Theme , se agregan atrubutos a la tabla  Instrumente weight y measures,pruevas en postman y se incorpora el archivo Local_Music_House.postman_collection co  los crud de la api,en todas las entidades se agrega el atributo registDate para gestionar la fecha d ecracion automatica .

--- music-house-back/src/main/java/com/musichouse/api/music/dto/dto_entrance/InstrumentDtoEntrance.java
+++ music-house-back/src/main/java/com/musichouse/api/music/dto/dto_entrance/InstrumentDtoEntrance.java
@@ -18,13 +18,27 @@ public class InstrumentDtoEntrance {
     @NotBlank(message = "El nombre del instrumento es obligatorio")
     @Size(max = 100, message = "El nombre del instrumento debe tener como máximo {max} caracteres")
     private String name;
+
     @Size(max = 255, message = "La descripción del instrumento debe tener como máximo {max} caracteres")
     private String description;
+
     @NotNull(message = "El precio de alquiler es obligatorio")
     @PositiveOrZero(message = "El precio de alquiler debe ser positivo o cero")
     private BigDecimal rentalPrice;
+
+    @NotNull(message = "El peso del instrumento es obligatorio")
+    @PositiveOrZero(message = "El peso debe ser positivo o cero")
+    private BigDecimal weight;
+
+    @NotBlank(message = "Las medidas  del instrumento es obligatorio")
+    private String measures;
+
     @NotNull(message = "el ID de la categoria no deve estar en blanco")
     private Long idCategory;
+
+    @NotNull(message = "el ID de la tematica no deve estar en blanco")
+    private Long idTheme;
+
     @NotNull(message = "Debe cargar al menos una imagen al crear el instrumento")
     private List<String> imageUrls;
 

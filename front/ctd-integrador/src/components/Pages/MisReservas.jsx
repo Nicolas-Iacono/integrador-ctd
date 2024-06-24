dad8be26fd5cc4a6174d0716284e9e0436bb3287
Nicolas Iacono <nicoiacono96@gmail.com>
2024-06-24 19:18:28 -0300

    mejorando btn de whatsapp

--- front/ctd-integrador/src/components/Pages/MisReservas.jsx
+++ front/ctd-integrador/src/components/Pages/MisReservas.jsx
@@ -1,37 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import {
-  Box,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TablePagination,
   TableRow,
-  Typography,
   Paper,
   Checkbox,
   IconButton,
-  Tooltip
-} from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import MainWrapper from '../common/MainWrapper';
-import { useNavigate } from 'react-router-dom';
+  Tooltip,
+  Typography
+} from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+import MainWrapper from '../common/MainWrapper'
 import {
   EnhancedTableHead,
-  EnhancedTableToolbar,
   getLabelDisplayedRows,
   isSelected,
   handleSort,
-  handleSelectAll,
   handleSelected,
   getEmptyRows,
   useVisibleRows
-} from '../Pages/Admin/common/tableHelper';
-import { MessageDialog } from '../common/MessageDialog';
-import { Loader } from '../common/loader/Loader';
-import { useAuthContext } from '../utils/context/AuthGlobal';
-import { ReservationApi } from '../../api/reservations';
+} from '../Pages/Admin/common/tableHelper'
+import { MessageDialog } from '../common/MessageDialog'
+import { Loader } from '../common/loader/Loader'
+import { useAuthContext } from '../utils/context/AuthGlobal'
+import { ReservationApi } from '../../api/reservations'
+import { Code } from '../../api/constants'
+import dayjs from 'dayjs'
 
 const headCells = [
   {
@@ -47,24 +44,25 @@ const headCells = [
     label: 'Imagen'
   },
   {
-    id: 'idInstrument',
+    id: 'instrumentName',
     numeric: true,
     disablePadding: false,
     label: 'Instrumento'
   },
+
   {
-    id: 'fechaInicio',
+    id: 'startDate',
     numeric: false,
     Date: true,
     disablePadding: false,
-    label: 'Reserva desde'
+    label: 'Inicio'
   },
   {
-    id: 'fechaFin',
+    id: 'endDate',
     numeric: false,
     Date: true,
     disablePadding: false,
-    label: 'Reserva Hasta'
+    label: 'Entrega'
   },
   {
     id: 'totalPrice',
@@ -84,234 +82,253 @@ const headCells = [
     disablePadding: false,
     label: 'Acciones'
   }
-];
+]
 
 const MisReservas = () => {
-  const [reservations, setReservations] = useState([]);
-  const [rows, setRows] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [order, setOrder] = useState('asc');
-  const [orderBy, setOrderBy] = useState('idUser');
-  const [selected, setSelected] = useState([]);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [showMessage, setShowMessage] = useState(false);
-  const [message, setMessage] = useState();
-  const [showCancelButton, setShowCancelButton] = useState(false);
-  const [onButtonPressed, setOnButtonPressed] = useState();
-  const navigate = useNavigate();
-  const { user } = useAuthContext();
+  const [reservations, setReservations] = useState()
+  const [rows, setRows] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [order, setOrder] = useState('asc')
+  const [orderBy, setOrderBy] = useState('number')
+  const [selected, setSelected] = useState([])
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+  const [showMessage, setShowMessage] = useState(false)
+  const [message, setMessage] = useState()
+  const [showCancelButton, setShowCancelButton] = useState(false)
+  const [onButtonPressed, setOnButtonPressed] = useState()
+  const { user } = useAuthContext()
 
   useEffect(() => {
-    if (!user) return;
-    const fetchReservations = async () => {
-      setLoading(true);
-      try {
-        const response = await ReservationApi.getReservationById(user.idUser);
-        const userReservations = response[0].data
-        setReservations(userReservations);
-        setRows(reservations);
+    if (!user) return
 
-      } catch (error) {
-        console.error('Error fetching reservations:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    getReservations()
+  }, [user])
 
-    fetchReservations();
-  }, [user]);
+  useEffect(() => {
+    if (!reservations) return
+
+    setRows(reservations.data)
+    setLoading(false)
+  }, [reservations])
+
+  const getReservations = () => {
+    setLoading(true)
+    ReservationApi.getReservationById(user.idUser, [])
+      .then(([data, _]) => {
+        setReservations(data)
+      })
+      .catch(([_, code]) => {
+        setReservations({ data: [] })
+      })
+  }
 
   const handleRequestSort = (event, property) => {
-    handleSort(event, property, orderBy, order, setOrderBy, setOrder);
-  };
-
-  const handleSelectAllClick = (event) => {
-    handleSelectAll(event, rows, 'idUser', setSelected);
-  };
+    handleSort(event, property, orderBy, order, setOrderBy, setOrder)
+  }
 
   const handleClick = (event, id) => {
-    handleSelected(event, id, selected, setSelected);
-  };
-
-
-
-  const handleEdit = (id) => {
-    navigate(`/path-to-edit-reservation/${id}`);
-  };
+    handleSelected(event, id, selected, setSelected)
+  }
 
   const handleConfirmDelete = () => {
-    setMessage('¿Desea eliminar esta reserva?');
-    setShowCancelButton(true);
-    setOnButtonPressed(true);
-    setShowMessage(true);
-  };
-
+    setMessage('¿Desea eliminar esta reserva?')
+    setShowCancelButton(true)
+    setOnButtonPressed(true)
+    setShowMessage(true)
+  }
 
   const handleClose = () => {
-    setShowMessage(false);
-    setSelected([]);
-  };
+    setShowMessage(false)
+    setSelected([])
+  }
 
-  const handleDelete = async () => {
-    setShowMessage(false);
-    const idReservation = selected[0];
+  const handleDelete = () => {
+    setShowMessage(false)
+    const idReservation = selected[0]
+    const reservation = rows.filter(
+      (rows) => rows.idReservation === idReservation
+    )
 
-    try {
-      // Simular la eliminación de la reserva
-      const updatedReservations = reservations.filter(res => res.idReservation !== idReservation);
-      setReservations(updatedReservations);
-      setRows(updatedReservations);
-      setMessage('Reserva eliminada exitosamente');
-    } catch (error) {
-      setMessage('No fue posible eliminar la reserva');
-      console.error('Error deleting reservation:', error);
-    } finally {
-      setShowCancelButton(false);
-      setOnButtonPressed(false);
-      setShowMessage(true);
-      setSelected([]);
-    }
-  };
+    if (!reservation.length > 0) return
+
+    ReservationApi.deleteReservation(
+      reservation[0]?.idInstrument,
+      user.idUser,
+      idReservation
+    )
+      .then(() => {
+        setMessage('Reserva eliminada exitosamente')
+        setShowCancelButton(false)
+        setOnButtonPressed(false)
+      })
+      .catch(([error, code]) => {
+        setMessage(
+          code === Code.NOT_FOUND
+            ? 'Reserva no encontrada.'
+            : 'No fue posible eliminar reserva.'
+        )
+        setShowCancelButton(false)
+        setOnButtonPressed(false)
+      })
+      .finally(() => {
+        setSelected([])
+        setShowMessage(true)
+        getReservations()
+      })
+  }
 
   const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+    setPage(newPage)
+  }
 
   const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
 
-  const emptyRows = getEmptyRows(page, rowsPerPage, rows);
+  const emptyRows = getEmptyRows(page, rowsPerPage, rows)
 
-  const visibleRows = useVisibleRows(rows, order, orderBy, page, rowsPerPage);
+  const visibleRows = useVisibleRows(rows, order, orderBy, page, rowsPerPage)
 
-  if (loading) return <Loader title="Cargando reservas" />;
+  if (loading) return <Loader title="Cargando reservas" />
 
   return (
-    <>
-      {!loading && (
-        <MainWrapper sx={{ width: '100%' }}>
-          <Paper
-            sx={{
-              display: { xs: 'none', lg: 'initial' },
-              width: '100%',
-              mb: 2
-            }}
+    <MainWrapper sx={{ width: '100%', minHeight: '90vh' }}>
+      <Paper
+        sx={{
+          width: '100%',
+          mb: 2
+        }}
+      >
+        <TableContainer>
+          <Table
+            sx={{ minWidth: { xs: '100%', md: 750 } }}
+            aria-labelledby="tableTitle"
+            size="medium"
           >
-         
-            <TableContainer>
-              <Table
-                sx={{ minWidth: 750 }}
-                aria-labelledby="tableTitle"
-                size="medium"
-              >
-                <EnhancedTableHead
-                  headCells={headCells}
-                  numSelected={selected.length}
-                  order={order}
-                  orderBy={orderBy}
-                  onSelectAllClick={handleSelectAllClick}
-                  onRequestSort={handleRequestSort}
-                  rowCount={rows.length}
-                />
-                <TableBody>
-                  {visibleRows &&
-                    visibleRows.map((row, index) => {
-                      const isItemSelected = isSelected(row.idUser, selected);
-                      const labelId = `enhanced-table-checkbox-${index}`;
-                      const isRowEven = index % 2 === 0;
-
-                      return (
-                        <TableRow
-                          hover
-                          role="checkbox"
-                          aria-checked={isItemSelected}
-                          tabIndex={-1}
-                          key={row.idReservation}
-                          selected={isItemSelected}
-                          sx={{
-                            cursor: 'pointer',
-                            backgroundColor: isRowEven ? '#fbf194' : 'inherit'
-                          }}
-                          onClick={(event) => handleClick(event, row.idUser)}
-                        >
-                          <TableCell padding="checkbox">
-                            <Checkbox
-                              color="primary"
-                              checked={isItemSelected}
-                              inputProps={{
-                                'aria-labelledby': labelId
-                              }}
-                            />
-                          </TableCell>
-                          <TableCell
-                            component="th"
-                            id={labelId}
-                            scope="row"
-                            padding="none"
-                            align="center"
-                          >
-                            {row.idReservation}
-                          </TableCell>
-                          <TableCell align="left"><img src={row.imageUrl} alt="" width="100px" /></TableCell>
-                          <TableCell align="left">{row.instrumentName}</TableCell>
-                          <TableCell align="left">{row.startDate}</TableCell>
-                          <TableCell align="left">{row.endDate}</TableCell>
-                          <TableCell align="left">$ {row.totalPrice}</TableCell>
-                          <TableCell align="left">{row.email}</TableCell>
-                          <TableCell align="left">
-                            <Tooltip title="Editar">
-                              <IconButton
-                                onClick={() => handleEdit(row.idUser)}
-                              >
-                                <EditIcon />
-                              </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Eliminar">
-                              <IconButton onClick={handleConfirmDelete}>
-                                <DeleteIcon />
-                              </IconButton>
-                            </Tooltip>
-                          </TableCell>
-                        </TableRow>
-                      );
-                    })}
-                  {emptyRows > 0 && (
-                    <TableRow
-                      style={{
-                        height: 53 * emptyRows
-                      }}
-                    >
-                      <TableCell colSpan={6} />
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              labelDisplayedRows={({ from, to, count }) => getLabelDisplayedRows(from, to, count)}
+            <EnhancedTableHead
+              headCells={headCells}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows?.length}
+              disableSelectAll
             />
-          </Paper>
-          <MessageDialog
-            open={showMessage}
-            onClose={handleClose}
-            onConfirm={onButtonPressed ? handleDelete : null}
-            showCancelButton={showCancelButton}
-            message={message}
-          />
-        </MainWrapper>
-      )}
-    </>
-  );
-};
+            <TableBody>
+              {visibleRows &&
+                visibleRows.map((row, index) => {
+                  const isItemSelected = isSelected(row.idUser, selected)
+                  const labelId = `enhanced-table-checkbox-${index}`
+                  const isRowEven = index % 2 === 0
 
-export default MisReservas;
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.idReservation}
+                      selected={isItemSelected}
+                      sx={{
+                        cursor: 'pointer',
+                        backgroundColor: isRowEven ? '#fbf194' : 'inherit'
+                      }}
+                      onClick={(event) => handleClick(event, row.idReservation)}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                        align="center"
+                      >
+                        {row.idReservation}
+                      </TableCell>
+                      <TableCell align="left">
+                        <img src={row.imageUrl} alt="" width="100px" />
+                      </TableCell>
+                      <TableCell align="left">{row.instrumentName}</TableCell>
+                      <TableCell sx={{ display: 'none' }} align="left">
+                        {row.idInstrument}
+                      </TableCell>
+                      <TableCell align="left">
+                        {dayjs(row.startDate).format('DD-MM-YYYY')}
+                      </TableCell>
+                      <TableCell align="left">
+                        {dayjs(row.endDate).format('DD-MM-YYYY')}
+                      </TableCell>
+                      <TableCell align="left">$ {row.totalPrice}</TableCell>
+                      <TableCell align="left">{row.email}</TableCell>
+                      <TableCell align="left">
+                        <Tooltip title="Eliminar">
+                          <IconButton onClick={handleConfirmDelete}>
+                            <DeleteIcon />
+                          </IconButton>
+                        </Tooltip>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows
+                  }}
+                >
+                  <TableCell colSpan={9} />
+                </TableRow>
+              )}
+              {page === 0 && rows.length === 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows
+                  }}
+                >
+                  <TableCell colSpan={9}>
+                    <Typography align="center">
+                      No se encontraron reservas
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelDisplayedRows={getLabelDisplayedRows}
+        />
+      </Paper>
+      <MessageDialog
+        title="Eliminar reserva"
+        message={message}
+        isOpen={showMessage}
+        buttonText="Ok"
+        onClose={handleClose}
+        onButtonPressed={() =>
+          onButtonPressed ? handleDelete() : handleClose()
+        }
+        showCancelButton={showCancelButton}
+      />
+    </MainWrapper>
+  )
+}
+
+export default MisReservas
